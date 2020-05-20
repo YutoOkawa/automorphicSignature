2d184ecee16e26ffe00e3d692d81e5589cb7878d
YutoOkawa <is0344rf@ed.ritsumei.ac.jp>
2020-05-20 17:14:03 +0900

    Automorphic Signatureのアルゴリズムの完成

--- main.py
+++ main.py
@@ -7,6 +7,13 @@ class Automorphic:
     def __init__(self, group):
         self.group = group
 
+    def changeMessageDH(self, pp, msg):
+        messageDh = {}
+        messageHash = self.group.hash(msg)
+        messageDh['M'] = pp['G'] ** messageHash
+        messageDh['N'] = pp['H'] ** messageHash
+        return messageDh
+
     def setup(self):
         pp = {}
         pp['G'] = self.group.random(G1)
@@ -20,16 +27,58 @@ class Automorphic:
         vk = {}
         x = group.random(ZR)
         sk = x
+        vk['X'] = pp['G'] ** x
+        vk['Y'] = pp['H'] ** x
         return vk, sk
 
-    def sign(self, pp, sk, m):
-        sign={}
+    def sign(self, pp, sk, message):
+        sign = {}
+        messageDh = self.changeMessageDH(pp, message)
+        c, r = self.group.random(ZR), self.group.random(ZR)
+        sign['A'] = (pp['K'] * (pp['T'] ** r) * messageDh['M']) ** (1 / (sk + c))
+        sign['C'] = pp['F'] ** c
+        sign['D'] = pp['H'] ** c
+        sign['R'] = pp['G'] ** r
+        sign['S'] = pp['H'] ** r
         return sign
 
-    def verify(self, pp, vk, m, sign):
-        return true
+    def verify(self, pp, vk, message, sign):
+        messageDh = self.changeMessageDH(pp, message)
+        pairCH = pair(sign['C'], pp['H'])
+        pairFD = pair(pp['F'], sign['D'])
+        if pairCH != pairFD:
+            print('e(C,H) != e(F,D)')
+            return False
+        else:
+            print('e(C,H) == e(F,D)')
+
+        pairRH = pair(sign['R'], pp['H'])
+        pairGS = pair(pp['G'], sign['S'])
+        if pairRH != pairGS:
+            print('e(R,H) != e(G,S)')
+            return False
+        else:
+            print('e(R,H) == e(G,S)')
+
+        pairAYD = pair(sig['A'], vk['Y'] * sign['D'])
+        pairKMH = pair(pp['K'] * messageDh['M'], pp['H'])
+        pairTS = pair(pp['T'], sig['S'])
+        if pairAYD != pairKMH * pairTS:
+            print('e(A,Y*D) != e(K*M,H) * e(T,S)')
+            return False
+
+        print('e(A,Y*D) == e(K*M,H) * e(T,S)')
+
+        return True
 
 if __name__ == "__main__":
     group = PairingGroup('MNT159')
     autinst = Automorphic(group)
     pp = autinst.setup()
+    print(pp)
+    vk, sk = autinst.keygen(pp)
+    print(vk)
+    print(sk)
+    sig = autinst.sign(pp, sk, 'hello world')
+    print(sig)
+    print(autinst.verify(pp, vk, 'hello world', sig))
